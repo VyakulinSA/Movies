6b8aa304109f249d87c7e2d551c5a3fc2a1da997
Vyakulin Sergey <Vyakulinsa@gmail.com>
2023-01-27 23:09:08 +0400

    init

---
+++ Movies/Presentation/Screens/MoviesList/View/MoviesListViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies
+//
+//  Created by Вякулин Сергей on 27.01.2023.
+//
+
+import UIKit
+
+class MoviesListViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		view.backgroundColor = .red
+	}
+
+
+}
+
